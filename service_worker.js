de20ed9dc426afb5e6c95832c8d869a986b6c814
Petter Amland <petter@rain-games.com>
2022-11-17 11:51:03 +0100

    ...

--- service_worker.js
+++ service_worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'index';
-const OFFLINE_URL = 'index.html';
-var urlsToCache = [
-  './',
-  './index.html'
-];
+const CACHE_NAME  = 'cmyk_swap-v1';
+const CONTENT_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/taptapir.js',
+  '/taptapir.css',
+]
 
-self.addEventListener('install', function(event) {
-    console.log('installing service_worker');
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME );
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(CONTENT_TO_CACHE);
+  })());
 });
