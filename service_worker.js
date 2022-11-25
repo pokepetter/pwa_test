6b9941efb42df4393d486d5b46fb2d710c603a0d
Petter Amland <petter@rain-games.com>
2022-11-25 17:07:52 +0100

    ...

--- service_worker.js
+++ service_worker.js
@@ -8,7 +8,14 @@ const CONTENT_TO_CACHE = [
 
 
 
+// This is the "Offline page" service worker
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -19,7 +26,8 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(CONTENT_TO_CACHE))
+      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.add_all(CONTENT_TO_CACHE))
   );
 });
 
@@ -36,3 +44,15 @@ self.addEventListener('fetch', (event) => {
         if (preloadResp) {
           return preloadResp;
         }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
