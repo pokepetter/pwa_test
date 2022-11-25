819e8dfc515f1bb38507ff7ae91c5b2bddcf94e7
Petter Amland <petter@rain-games.com>
2022-11-25 16:10:36 +0100

    ...

--- service_worker.js
+++ service_worker.js
@@ -6,11 +6,33 @@ const CONTENT_TO_CACHE = [
   '/taptapir.css',
 ]
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME );
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(CONTENT_TO_CACHE);
-  })());
+
+
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CONTENT_TO_CACHE))
+  );
+});
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
